9aae43919f2197d4b4f9cecea37a4f6e4613ffdd
Langelozzi <lucasangelozzi32@gmail.com>
2025-10-28 11:49:54 -0700

    Finished first working api scraping

--- api/app/modules/scraper/service.py
+++ api/app/modules/scraper/service.py
@@ -1,8 +1,8 @@
 from pydantic import HttpUrl
+from app.shared.helpers.dict_helpers import rename_key
 from app.shared.models.html import DOMTree
-from app.shared.models.scrape import RetrievalInstruction, ScrapeConfig
+from app.shared.models.scrape import NodeOutput, RetrievalInstruction, ScrapeConfig
 from app.shared.models.scrape import ScrapedDataset
-from app.shared.data.scraped_data import get_scraped_data
 
 from scrapegoat import HTMLNode, Sheepdog, Shepherd
 
@@ -17,10 +17,24 @@ def build_dom_tree(url: HttpUrl) -> DOMTree:
 
 def scrape(config: ScrapeConfig) -> ScrapedDataset:
     tree_root = __get_tree_root(str(config.url))
-    query = __build_goatspeek_query(config.retrieval_instructions)
-    print(tree_root)
-    print(query)
-    return get_scraped_data(config.url)
+
+    results = []
+    for instruction in config.retrieval_instructions:
+        data = __scrape_single(tree_root, instruction)
+        results.extend(data)
+
+    return ScrapedDataset(url=config.url, data=results)
+
+
+### Private helper function ###
+def __scrape_single(
+    tree_root: HTMLNode, instruction: RetrievalInstruction
+) -> list[dict]:
+    query = __build_goatspeek_query(instruction)
+    scraped_nodes = shepherd.lead_goat(tree_root, query)
+    raw_results = [node.to_dict() for node in scraped_nodes]
+    mapped_results = __remap_dict_keys(raw_results, instruction.output)
+    return mapped_results
 
 
 def __get_tree_root(url: str) -> HTMLNode:
@@ -28,9 +42,20 @@ def __get_tree_root(url: str) -> HTMLNode:
     return shepherd.sow(seed)
 
 
-def __build_goatspeek_query(instructions: list[RetrievalInstruction]) -> str:
-    query = ""
+def __build_goatspeek_query(instruction: RetrievalInstruction) -> str:
+    query = instruction.node_query
+    extract_statement = __build_extract_statement_from_output(instruction.output)
 
-    print(instructions)
+    return f"{query}{extract_statement}"
 
-    return query
+
+def __build_extract_statement_from_output(output: NodeOutput) -> str:
+    return f"EXTRACT {output.location};"
+
+
+def __remap_dict_keys(raw_results: list[dict], output: NodeOutput) -> list[dict]:
+    default_key = output.location.removeprefix(
+        "@"
+    )  # TODO: This is likely to change so we don't have to remove @
+    new_key = output.key
+    return [rename_key(res, default_key, new_key) for res in raw_results]
--- sdk/src/scrapegoat/classes/html_node.py
+++ sdk/src/scrapegoat/classes/html_node.py
@@ -1,7 +1,5 @@
 """ """
 
-from typing import Any
-
 
 class HTMLNode:
     """ """
@@ -29,7 +27,7 @@ class HTMLNode:
         raw: str,
         tag_type: str,
         has_data: bool = False,
-        html_attributes: dict[str, Any] | None = None,
+        html_attributes: dict[str, any] = None,
         body: str = "",
         parent=None,
     ):
@@ -45,17 +43,11 @@ class HTMLNode:
         self.children = []
         self.retrieval_instructions = ""
         self.parent = parent
-<<<<<<< HEAD
-
-    def to_dict(self) -> dict:
-        """ """
-=======
         self.extract_fields = None
         self.extract_flags = None
-    
+
     def to_dict(self) -> str:
-        """
-        """
+        """ """
         dict_representation = {}
         if self.extract_fields is not None:
             for field in self.extract_fields:
@@ -74,7 +66,6 @@ class HTMLNode:
                     elif field == "body":
                         dict_representation["body"] = self.body
             return dict_representation
->>>>>>> 8ac1ab97f50a12536e1258424bb2da98ea7eaf15
         return {
             "id": self.id,
             "raw": self.raw,
@@ -85,23 +76,13 @@ class HTMLNode:
             "children": [child.to_dict() for child in self.children],
             "retrieval_instructions": self.retrieval_instructions,
             "parent": self.parent.id if self.parent else None,
-<<<<<<< HEAD
+            "extract_fields": self.extract_fields,
+            "extract_flags": self.extract_flags,
         }
 
     def to_string(self) -> str:
         """ """
-        return f"HTMLNode(id={self.id}, tag_type={self.tag_type}, has_data={self.has_data}, html_attributes={self.html_attributes}, body='{self.body}', children={len(self.children)}, parent={self.parent.id if self.parent else None}, retrieval_instructions='{self.retrieval_instructions}')"
-=======
-            "extract_fields": self.extract_fields,
-            "extract_flags": self.extract_flags,
-        }
-                
-
-    def to_string(self) -> str:
-        """
-        """
         return str(self.to_dict())
->>>>>>> 8ac1ab97f50a12536e1258424bb2da98ea7eaf15
 
     def to_html(self, indent=0) -> str:
         """ """
@@ -150,7 +131,7 @@ class HTMLNode:
             current = current.parent
         return ancestors
 
-    def get_descendants(self, tag_type: str | None = None, **html_attributes) -> list:
+    def get_descendants(self, tag_type: str = None, **html_attributes) -> list:
         """ """
         descendants = []
         for child in self.children:
@@ -174,13 +155,9 @@ class HTMLNode:
         if self.html_attributes.get(key) is None:
             return False
         return value in self.html_attributes.get(key)
-<<<<<<< HEAD
 
-=======
-    
     def has_attribute(self, key, value=None) -> bool:
-        """
-        """
+        """ """
         if key == "tag_type":
             if value is None:
                 return self.tag_type is not None
@@ -198,8 +175,7 @@ class HTMLNode:
                 return self.body is not None
             return self.body == value
         return False
-    
->>>>>>> 8ac1ab97f50a12536e1258424bb2da98ea7eaf15
+
     def is_descendant_of(self, tag_type) -> bool:
         """ """
         return any(ancestor.tag_type == tag_type for ancestor in self.get_ancestors())
@@ -209,14 +185,12 @@ class HTMLNode:
         self.retrieval_instructions = instruction
 
     def set_extract_instructions(self, fields: list, flags: list):
-        """
-        """
+        """ """
         self.extract_fields = fields
         self.extract_flags = flags
 
     def clear_extract_instructions(self):
-        """
-        """
+        """ """
         self.extract_fields = None
         self.extract_flags = None
 
